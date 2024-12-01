a6a02540966412251649976f990c4ac2a582182c
Durgesh Kumar Meena <58581435+durgeshmeena@users.noreply.github.com>
2024-12-01 14:31:10 +0530

    add servmux router

---
+++ internal/webserver/router/servemux.go
@@ -0,0 +1,57 @@
+package router
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/durgeshmeena/envoy-gateway-controller/internal/webserver/handler"
+	"github.com/go-logr/logr"
+)
+
+
+
+type User struct {
+	User string `json:"user"`
+
+}
+
+
+
+func GetRouters(webLog logr.Logger) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello World"))
+	})
+
+	mux.HandleFunc("/user/{username}", func(w http.ResponseWriter, r *http.Request) {
+		userName := r.PathValue("username")
+		fmt.Fprintf(w, "Hello %s", userName)
+	})
+
+	mux.HandleFunc("POST /user", func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		webLog.Info("Received POST request", "time", time.Now())
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			webLog.Error(err, "Failed to decode request body")
+			return
+		}
+		// fmt.Fprintf(w, "User: %s", user.User)
+		webLog.Info("User", "user", user.User)
+	})
+
+	// mux.HandleFunc("POST /btp/create", handler.CreateClientBTP)
+	mux.Handle("POST /btp/create", &handler.CreateClientBTPHandler{Logger: &webLog})
+
+
+	return mux
+}
